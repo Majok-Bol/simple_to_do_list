2c0718016c65f8b5db9e3daddff61e59b789e6e2
Majok-Bol <bolmajok001@gmail.com>
2025-03-25 10:01:19 +0300

    added add and delete buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,117 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
+void main(){
+  runApp(MyApp());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home:ShoppingList(),
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ShoppingList extends StatefulWidget{
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  ShoppingListState createState()=>ShoppingListState();
 }
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+class ShoppingListState extends State<ShoppingList>{
+  //create variable to store list items
+  final List<String>itemsToAdd=[];
+  //keep track of text controller
+  final TextEditingController itemController=TextEditingController();
+  //keep track of index of items
+  //so that if there is item to be edited
+  //then it is edited based on the index
+  int?editingIndex;
+  //function to add item to list
+  void addItem(){
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+      //check if there text field is not empty
+      //if not empty
+      //check if there is item to be edited
+      if(itemController.text.isNotEmpty){
+        //check editing index to edit the item based on index
+        if(editingIndex==null){
+          // add item in the text input field
+          itemsToAdd.add(itemController.text);
+        }else{
+          //after editing the item then add it to the list
+          itemsToAdd[editingIndex!]=itemController.text;
+          //reset the editing index
+          editingIndex=null;
+        }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+      }
+
+      //clear the input field
+      itemController.clear();
+
+    });
+
+  }
+  void deleteItems(){
+    //clear the item list
+    setState(() {
+      itemsToAdd.clear();
+    });
+
+
+  }
+  void editItems(index){
+    setState(() {
+      //get the index
+      //edit the text
+      itemController.text=itemsToAdd[index];
+      editingIndex=index;
+    });
+
+
+  }
+@override
+  Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      appBar: AppBar(title: Text('Simple to-do-list Application',style: TextStyle(color: Colors.red,fontSize: 20),),centerTitle: true,),
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        margin: EdgeInsets.symmetric(vertical: 20,horizontal: 20),
+        // color: Colors.grey,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          children: [
+            Row(children: [
+              Expanded(child: TextField(
+                controller: itemController,
+                decoration: InputDecoration(
+                  hintText: 'Enter text',
+                  border: OutlineInputBorder(),
+                ),
+              )),
+            ],),
+                SizedBox(height: 20,),
+                Row(
+                  children: [
+                    Expanded(child: ElevatedButton(onPressed:addItem,style:ElevatedButton.styleFrom(backgroundColor: Colors.green),child: Text(editingIndex==null?"Add":"Update"),),),
+                    SizedBox(width: 10,),
+                    // Expanded(child: ElevatedButton(onPressed:()=>editItems,style:ElevatedButton.styleFrom(backgroundColor: Colors.grey),child: Text(''
+                    //     'Edit'),),),
+                    SizedBox(width: 10,),
+                    Expanded(child: ElevatedButton(onPressed:deleteItems,style:ElevatedButton.styleFrom(backgroundColor: Colors.red),child: Text(''
+                        'Delete'),),),
+                  ],
+                ),
+
+                Expanded(child: ListView.builder(
+                  itemCount: itemsToAdd.length,
+
+                    itemBuilder:(context,index){
+                    return ListTile(title: Text(itemsToAdd[index]),trailing: IconButton(onPressed:()=>editItems(index), icon:Icon(Icons.edit,color: Colors.blue,)),);
+
+                })),
+
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
+}
+
 }
